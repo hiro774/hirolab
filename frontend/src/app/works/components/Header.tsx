9041a77f1ac05d2e616ff9eb2bf29c4e4ebec541
hiro <wi2llnoutput@yahoo.co.jp>
2025-04-15 14:48:10 +0900

    特殊文字エスケープ

--- frontend/src/app/works/components/Header.tsx
+++ frontend/src/app/works/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
 
       {/* テキスト（画像の下） */}
       <p className="text-gray-600 max-w-2xl mx-auto">
-        Here are some of the projects I've worked on. details.
+        Here are some of the projects I&apos;ve worked on. details.
       </p>
     </div>
   );
