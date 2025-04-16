bb9541eb8a6edf617a29169eab6bf226019734d4
hiro <wi2llnoutput@yahoo.co.jp>
2025-04-17 04:12:15 +0900

    余白ミス修正

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" data-theme="light" className="scroll-smooth">
-      <body className="h-[100svh]">
+      <body className="min-h-screen">
         <div>
           <input
             id="my-drawer-3"
